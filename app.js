ec4b1b84cf5444072312e279039dcc4cf040caac
usiddiki97 <usiddiki97@gmail.com>
2020-11-30 15:52:50 -0500

    Finish up till Logout action

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const mongoose = require('mongoose');
 const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users")
-const tweets = require("./routes/api/tweets")
 const User = require('./models/User');
 const bodyParser = require('body-parser');
 const passport = require('passport');
@@ -32,7 +31,6 @@ app.get("/", (request, response) => {
 });
 
 app.use("/api/users", users);
-app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
