ac590fdbe6c9a12603f29cbc6e19ac90fd24aab0
usiddiki97 <usiddiki97@gmail.com>
2020-11-30 20:22:25 -0500

    Finish app.js and install proper dependancies

---
+++ frontend/src/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { Switch } from 'react-router-dom';
+
+// import MainPage from './main/main_page';
+
+const App = () => (
+    // <Switch>
+    //     <AuthRoute exact path="/" component={MainPage} />
+    // </Switch>
+    <div>Hello</div>
+);
+
+export default App;
---
+++ frontend/src/reducers/session_reducer.js
@@ -0,0 +1,20 @@
+import { RECEIVE_USER_LOGOUT } from '../actions/session_actions';
+
+const initialState = {
+    isAuthenticated: false,
+    user: {}
+}
+
+export default function(state = initialState, action) {
+    Object.freeze(initialState);
+
+    switch (action.type) {
+        case RECEIVE_USER_LOGOUT:
+            return {
+                isAuthenticated: false,
+                user: undefined
+            };
+        default:
+            return state;
+    }
+}
