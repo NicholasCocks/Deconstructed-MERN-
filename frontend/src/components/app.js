1c44944e8d8ac04a9ac58e3ecf59328dc02a0838
nickc <nick@caveat.nyc>
2020-12-02 09:35:58 -0500

    fixed appjs switch statement bug

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -5,17 +5,16 @@ import NavBarContainer from './nav/nav_bar_container';
 import LoginFormContainer from './session/login_form_container';
 import QuestionFormContainer from './questions_form/questions_form_container';
 import SignupFormContainer from './session/signup_form_container';
-
 import MainPage from './main/main_page';
 
 const App = () => (
         <div>
                 <NavBarContainer />
                 <Switch>
-                        <Route path="/" component={QuestionFormContainer} />
-                        <Route exact path="/" component={MainPage} />
-                        <AuthRoute exact path="/login" component={LoginFormContainer} />
-                        <AuthRoute exact path="/signup" component={SignupFormContainer} />
+                        {/* <Route path="/" component={QuestionFormContainer} /> */}
+                        <AuthRoute path="/login" component={LoginFormContainer} />
+                        <AuthRoute path="/signup" component={SignupFormContainer} />
+                        <Route path="/" component={MainPage} />
                 </Switch>
         </div>
 );
