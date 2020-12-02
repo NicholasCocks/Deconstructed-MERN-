d287e33721c6f0dcc3f1a4bc562faf3466d788bb
mchowdhury <muz.c98@gmail.com>
2020-12-02 12:10:06 -0500

    Added scripts for heroku

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const questions = require('./routes/api/questions');
 const dataclasses = require('./routes/api/dataclasses');
+const path = require('path');
 
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -28,6 +29,14 @@ app.use("/api/users", users);
 app.use("/api/questions", questions);
 app.use("/api/dataclasses", dataclasses)
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
+
