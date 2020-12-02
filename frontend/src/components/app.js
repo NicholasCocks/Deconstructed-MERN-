567c97ae923b1ba0cbc687d82104dbe609877e2d
nickc <nick@caveat.nyc>
2020-12-01 21:28:39 -0500

    basic question/dataclass state setup

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import { Switch } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import NavBarContainer from './nav/nav_bar_container';
 import LoginFormContainer from './session/login_form_container';
+import QuestionFormContainer from './questions_form/questions_form_container';
 import SignupFormContainer from './session/signup_form_container';
+
 import MainPage from './main/main_page';
 
 const App = () => (
-    <div>
-        <NavBarContainer />
-        <Switch>
-            <AuthRoute exact path="/" component={MainPage} />
-            <AuthRoute exact path="/login" component={LoginFormContainer} />
-            <AuthRoute exact path="/signup" component={SignupFormContainer} />
-        </Switch>
-    </div>
+        <div>
+                <NavBarContainer />
+                <Switch>
+                        <Route path="/" component={QuestionFormContainer} />
+                        <Route exact path="/" component={MainPage} />
+                        <AuthRoute exact path="/login" component={LoginFormContainer} />
+                        <AuthRoute exact path="/signup" component={SignupFormContainer} />
+                </Switch>
+        </div>
 );
 
 export default App;
