16e21eda62bd30135bd1fc522de69563b8fec455
mchowdhury <muz.c98@gmail.com>
2020-12-03 15:15:35 -0500

    working on seeding

--- routes/api/questions.js
+++ routes/api/questions.js
@@ -9,83 +9,101 @@ router.get('/seed', (req, res) => {
     Question.find({ question: "google" })
         .then(question => {
             Dataclass.find({
-                class: "eating habits"})
+                class: "age groups"})
                 .then(dataclass => {
-                    dataclass.companiesCollecting.push(question._id)
-                    dataclass.save()
+                    debugger
+                    dataclass[0].companiesCollecting.push(question[0]._id)
+                    dataclass[0].save().then(res.json({msg: "success!"}))
 
-                    question.dataclassCollection.push(dataclass._id)
+                    question[0].dataclassCollection.push(dataclass[0]._id)
 
-                    dataclass.description = "asdfja"
-                    question.save()
+                    // dataclass.description = ""
+                    question[0].save()
                         .then(question => res.json(question))
                         .catch(err => res.json(err))
                 })
         })
 })
 
+// router.get('/seed', (req, res) => {
 
-router.get('/test', (req, res) => {
-    debugger
-    res.json({
-        msg: "this is the question route"
-    })
-})
+//     Question.find({ question: "google" })
+//         .then(questionArr => {
+//             Dataclass.find({ class: "email messages" })
+//                 .then(dataclassArr => {
+//                     dataclassArr[0].companiesCollecting.push(questionArr[0]._id)
+//                     dataclassArr[0].save().then()
 
-//return all questions (in an array)
-router.get('/', (req, res) => {
-    Question.find()
-        .then(questions => {
-            const obj = {};
-            questions.forEach(question => {
-                obj[question._id] = question
-            })
-            return res.json(obj)
-        })
-        .catch(err => console.log(err))
-})
+//                     questionArr[0].dataclassCollection.push(dataclassArr[0]._id)
+//                     questionArr[0].save()
+//                         .then(question => res.json(question))
+//                         .catch(err => res.json(err))
+//                 })
+//         })
+// })
 
-//create questions
-router.post('/new', (req, res) => {
-    const { errors, isValid } = validateQuestionInput(req.body);
+// router.get('/test', (req, res) => {
+//     debugger
+//     res.json({
+//         msg: "this is the question route"
+//     })
+// })
 
-    if (!isValid) return res.status(400).json(errors)
+// //return all questions (in an array)
+// router.get('/', (req, res) => {
+//     Question.find()
+//         .then(questions => {
+//             const obj = {};
+//             questions.forEach(question => {
+//                 obj[question._id] = question
+//             })
+//             return res.json(obj)
+//         })
+//         .catch(err => console.log(err))
+// })
 
-    const toLower = req.body.question.toLowerCase()
-    Question.findOne({ question: toLower })
-        .then(question => {
-            if (question) {
-                errors.question = 'Question already exists'
-                return res.status(400).json(errors)
-            } else {
-                const newQuestion = new Question({ question: toLower })
-                newQuestion.save()
-                    .then(question => res.json(question))
-                    .catch(error => console.log(error))
-            }
-        })
+// //create questions
+// router.post('/new', (req, res) => {
+//     const { errors, isValid } = validateQuestionInput(req.body);
 
-})
+//     if (!isValid) return res.status(400).json(errors)
 
-router.patch('/update', (req, res) => {
-    const { errors, isValid } = validateQuestionInput(req.body);
+//     const toLower = req.body.question.toLowerCase()
+//     Question.findOne({ question: toLower })
+//         .then(question => {
+//             if (question) {
+//                 errors.question = 'Question already exists'
+//                 return res.status(400).json(errors)
+//             } else {
+//                 const newQuestion = new Question({ question: toLower, url: req.body.url })
+//                 newQuestion.save()
+//                     .then(question => res.json(question))
+//                     .catch(error => console.log(error))
+//             }
+//         })
 
-    if (!isValid) return res.status(400).json(errors)
+// })
 
-    const toLower = req.body.question.toLowerCase()
-    Question.findOne({ question: toLower })
-        .then(question => {
-            if (question) {
-                question.dataclassCollection = req.body.class
-                question.save()
-                    .then(question => res.json(question))
-                    .catch(error => console.log(error))
-            } else {
-                errors.class = "can't find this question"
-                return res.status(400).json(errors)
-            }
-        })
-})
+// router.patch('/update', (req, res) => {
+//     const { errors, isValid } = validateQuestionInput(req.body);
+
+//     if (!isValid) return res.status(400).json(errors)
+
+//     const toLower = req.body.question.toLowerCase()
+//     Question.findOne({ question: toLower })
+//         .then(question => {
+//             if (question) {
+//                 question.dataclassCollection = req.body.class
+//                 question.url = req.body.url
+//                 question.save()
+//                     .then(question => res.json(question))
+//                     .catch(error => console.log(error))
+//             } else {
+//                 errors.class = "can't find this question"
+//                 return res.status(400).json(errors)
+//             }
+//         })
+// })
 
 module.exports = router;
 
