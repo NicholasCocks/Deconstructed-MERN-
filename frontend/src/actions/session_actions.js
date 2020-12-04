a2165c65c59959487378fbd471c94d8620f7a211
mchowdhury <muz.c98@gmail.com>
2020-12-04 12:10:07 -0500

    typo in app js

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -47,6 +47,18 @@ export const signup = user => dispatch => {
     ))
 };
 
+// export const signup = (user) => (dispatch) => {
+//     return APIUtil.signup(user)
+//         .then((res) => {
+//             const { token } = res.data;
+//             localStorage.setItem("jwtToken", token);
+//             APIUtil.setAuthToken(token);
+//             const decoded = jwt_decode(token);
+//             dispatch(receiveCurrentUser(decoded));
+//         })
+//         .catch((err) => dispatch(receiveErrors(err.response.data))
+//         );
+
 // Upon login, set the session token and dispatch the current user. Dispatch errors on failure.
 export const login = user => dispatch => {
   
