6bb39bfdc4f728edcf336793f8aa7c0be66a4054
mchowdhury <muz.c98@gmail.com>
2020-12-04 11:54:04 -0500

    fixed app js

--- frontend/src/reducers/session_api_reducer.js
+++ frontend/src/reducers/session_api_reducer.js
@@ -28,6 +28,8 @@ export default function (state = initialState, action) {
       return {
         ...state,
         isSignedIn: true,
+        isAuthenticated: !!action.currentUser,
+        user: action.currentUser
       };
     case RECEIVE_ANSWERS:
        
