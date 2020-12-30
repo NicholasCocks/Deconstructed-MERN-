f62449c20d4418646c1f2d71ed355facdfa5ae92
nickc <nick@caveat.nyc>
2020-12-30 12:55:45 -0500

    CSS Animations preserved

--- frontend/src/components/tasks/task_item.jsx
+++ frontend/src/components/tasks/task_item.jsx
@@ -6,32 +6,31 @@ class TaskItem extends React.Component{
     constructor(props) {
         super(props)
         this.state = {open: false}
-        this.toggle = this.toggle.bind(this);
+        this.onToggle = this.onToggle.bind(this);
         this.completeTask = this.completeTask.bind(this);
     }
 
-    toggle() {
-        this.state.open ? this.setState({open: false}) : this.setState({open: true})
-    }
+    onToggle = () => this.setState({open: !this.state.open})
 
     completeTask() {
         this.props.updateTask(this.props.task)
     }
 
     render() {
-        let openClass = this.state.open ? 'task_item_open' : 'task_item_closed';
         let rotate = this.state.open ? 0 : -90;
+        
         return (
             <li className="task_item_container" >
                 <div className="task_item_header"  >
-                    <div onClick={() => this.toggle()} className={`task_item_name`}>
-                        <FontAwesomeIcon icon={faCaretDown}  transform={{ rotate: rotate }}/>
+                    <div onClick={this.onToggle} className={`task_item_name`}>
+                        <FontAwesomeIcon icon={faCaretDown}  className={this.state.open ? "caret_down" : "caret_up"}/>
                         <p>{this.props.question.charAt(0).toUpperCase() + this.props.question.slice(1)}</p>
                     </div>
                     <FontAwesomeIcon color={this.props.task.isComplete ? "green" : "lightgray"} icon={this.props.task.isComplete ? faCheckCircle : faCircle} onClick={() => this.completeTask()}/>
                 </div>
-                <div className={openClass}>
+                <div className={this.state.open ? 'task_item task_item_open' : 'task_item'}>
                     <a href={this.props.url} target="_blank" >More Info</a>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, distinctio iure omnis itaque voluptatibus tenetur corporis consequuntur tempore facere? Iste a aspernatur, eos corrupti porro saepe laborum repellat rerum quisquam!</p>
                 </div>
             </li>
         )
