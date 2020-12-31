a876f2d71e41e77e59b8ba9fc82e58c8a7acd4a4
nickc <nick@caveat.nyc>
2020-12-31 12:49:36 -0500

    about section formatted nicely

--- frontend/src/components/about_section/about_section.jsx
+++ frontend/src/components/about_section/about_section.jsx
@@ -24,16 +24,17 @@ export default function AboutSection(props) {
         <FontAwesomeIcon icon={faAngleLeft} onClick={() => openModal(!modal)} className={modal ? "task_arrow task_arrow_expanded" : "task_arrow task_arrow_collapse"} />
         <h4 className="about_title">About</h4>
         <div className="about_body">
-            <p className="about_desc"><i> Deconstructed is a dynamic app to bring light to how much data is being collected 
+            <p className="about_desc"><i>Deconstructed</i> is a dynamic app to bring light to how much data is being collected 
                 from Big Tech companies we interact with daily. Nowadays with everything being online, 
-                it’s more important than ever to protect your data and privacy. We aim to let users 
+                it’s more important than ever to protect your data and privacy. <br /><br /> <p>We aim to let users 
                 engage with what data is being gathered on them and empowering them to take control of 
                 heir data. This app was designed to be used anonymously, but with the added functionality 
                 of a curated task list for each logged in user. This task list provides detailed instructions 
-                for each user to make their data more secure.</i></p>
+                for each user to make their data more secure.</p></p>
             <h4 className="about_howto_title">How to Use: </h4>
                 <ul className="howto_list">
                     <li>Under Accounts, we have a list of all the major platforms the average consumer would have.</li>
+                    
                     <li>Begin by clicking on the companies which you personally have accounts with!</li>
                     <li>Click on any company displayed on the chart to see specific data classes it may be collecting</li>
                     <li>Deciphering the color scale: 
