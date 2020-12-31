6b8c9778e99c7a21c697e63e2f2d7e756d1c6c04
mchowdhury <muz.c98@gmail.com>
2020-12-31 00:09:01 -0500

    merge

--- frontend/src/components/tasks/task_item.jsx
+++ frontend/src/components/tasks/task_item.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown, faCheckCircle, faCircle } from '@fortawesome/free-solid-svg-icons';
 
-class TaskItem extends React.Component{ 
+class TaskItem extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {open: false}
+        this.state = { open: false }
         this.onToggle = this.onToggle.bind(this);
         this.completeTask = this.completeTask.bind(this);
     }
 
-    onToggle = () => this.setState({open: !this.state.open})
+    onToggle = () => this.setState({ open: !this.state.open })
 
     completeTask() {
         this.props.updateTask(this.props.task)
@@ -22,14 +22,15 @@ class TaskItem extends React.Component{
         return (
             <li className="task_item_container" >
                 <div className="task_item_header" onClick={this.onToggle}>
-                    <div  className={`task_item_name`}>
-                        <FontAwesomeIcon icon={faCaretDown}  className={this.state.open ? "caret_down" : "caret_up"}/>
+                    <div className={`task_item_name`}>
+                        <FontAwesomeIcon icon={faCaretDown} className={this.state.open ? "caret_down" : "caret_up"} />
                         <p>{this.props.question.question.charAt(0).toUpperCase() + this.props.question.question.slice(1)}</p>
                     </div>
-                    <FontAwesomeIcon color={this.props.task.isComplete ? "green" : "lightgray"} icon={this.props.task.isComplete ? faCheckCircle : faCircle} onClick={() => this.completeTask()}/>
+                    <FontAwesomeIcon color={this.props.task.isComplete ? "green" : "lightgray"} icon={this.props.task.isComplete ? faCheckCircle : faCircle} onClick={() => this.completeTask()} />
                 </div>
-                <div className={openClass}>
-                    <a href={this.props.url} target="_blank" rel="noreferrer">More Info</a>
+                <div className={this.state.open ? 'task_item task_item_open' : 'task_item'}>
+                    <a href={this.props.url} target="_blank" >Link.</a>
+                    <p>{this.props.question.instructions}</p>
                 </div>
             </li>
         )
